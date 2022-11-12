4c0314ea4a48291a6b86469e5e45592dd37492ed
TheMVProgrammer <franklin0415@hotmail.com>
2022-11-12 12:22:10 -0400

    Adding navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import Start from "./components/start";
-import Questions from "./components/questions";
+import UpBlob from "./img/up-blob.svg";
+import DownBlob from "./img/down-blob.svg";
+import {useNavigate} from 'react-router-dom';
 
-function App() {
+export default function App() {
+
+   const navigate = useNavigate();
+
+  const navigateToQuestions = () => {
+      // 👇️ navigate to /questions
+      navigate('/questions');
+  };
+
   return (
-    <div className="App">
-      {/* <Start/> */}
-      <Questions/>
-    </div>
+      <div className="App">
+        <div className= {`start-container`}>
+          <img src={UpBlob} alt="upper blob" className="up-blob blob"/>
+          <h1 className="start-title">Quizzical</h1>
+          <p className="start-desc">Quizzical combines fun and learning. Your general knowledge will be tested regarding a variety of categories. Enjoy!</p>
+          <button 
+              onClick={navigateToQuestions}
+              className="button-start">
+              Start quiz
+          </button>
+          <img src={DownBlob} alt="lower blob" className="down-blob blob"/>
+        </div>     
+      </div>   
   );
 }
 
-export default App;
+ 
+
+
--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Questions from './components/questions';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/questions" element={<Questions />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
