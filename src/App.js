0bb6d9af9fc5c10395268dcee1b8c87d2e848f03
TheMVProgrammer <franklin0415@hotmail.com>
2022-11-19 19:33:00 -0400

    I did everything wrong, this is a new beginning.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import UpBlob from "./img/up-blob.svg";
-import DownBlob from "./img/down-blob.svg";
-import {useNavigate} from 'react-router-dom';
+import UpBlob from "./assets/img/up-blob.svg";
+import DownBlob from "./assets/img/down-blob.svg";
 
 export default function App() {
 
-   const navigate = useNavigate();
-
-  const navigateToQuestions = () => {
-      // 👇️ navigate to /questions
-      navigate('/questions');
-  };
-
   return (
       <div className="App">
-        <div className= {`start-container`}>
-          <img src={UpBlob} alt="upper blob" className="up-blob blob"/>
-          <h1 className="start-title">Quizzical</h1>
-          <p className="start-desc">Quizzical combines fun and learning. Your general knowledge will be tested regarding a variety of categories. Enjoy!</p>
-          <button 
-              onClick={navigateToQuestions}
-              className="button-start">
-              Start quiz
-          </button>
-          <img src={DownBlob} alt="lower blob" className="down-blob blob"/>
-        </div>     
+            
       </div>   
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Questions from './components/questions';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/questions" element={<Questions />} />
-      </Routes>
-    </Router>
+    <App />       
   </React.StrictMode>
 );
